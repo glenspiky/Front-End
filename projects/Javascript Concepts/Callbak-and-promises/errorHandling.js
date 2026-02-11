cd990ee31f4bdf94a91e89cf77ca5ce35e8cfca7
glenspiky <glenbarasa4@gmail.com>
2026-02-11 11:25:46 +0300

    Added notes and examples for Error handling in promises

---
+++ projects/Javascript Concepts/Callbak-and-promises/errorHandling.js
@@ -0,0 +1,44 @@
+const cart = ["shues", "pants", "shirts"];
+
+createOder(cart)
+  .then(function (orderId) {
+    console.log(orderId);
+    return orderId;
+    //procedToPayment(orderId);
+  })
+  .then((orderId) => {
+    return procedToPayment(orderId);
+  })
+  .then((paymentInfo) => {
+    console.log("payment info");
+  });
+
+//producer
+function createOder(cart) {
+  const pr = new Promise((resolve, reject) => {
+    //creteOder
+    //validateCart
+    //orderId
+    if (!validateCart(cart)) {
+      const err = new Error("cart is invalid");
+      return reject(err);
+    }
+    //logic for create order
+    const orderId = "1234";
+    if (orderId) {
+      setTimeout((order) => {
+        resolve(orderId);
+      }, 5000);
+    }
+  });
+  return pr;
+}
+
+function validateCart() {
+  return false;
+}
+function procedToPayment(orderId) {
+  return new Promise((resolve, reject) => {
+    resolve("payment seccesful");
+  });
+}
